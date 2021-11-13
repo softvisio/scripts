2617ff73ffa2fcba5c59454a04e86073acbaa932
zdm <zdm@softvisio.net>
2021-11-13 08:55:29 +0200

    fix: ubuntu init

--- setup-host.sh
+++ setup-host.sh
@@ -55,7 +55,7 @@ EOF
     apt full-upgrade
 
     # install common packages
-    apt install -y bash-completion ca-certificates tar bzip2
+    apt install -y bash-completion ca-certificates tar bzip2 ncurses-term
 
     # clean old kernels
     apt autoremove
