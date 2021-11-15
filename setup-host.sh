2fd0c83e305a4ed9b35d251d586afb7ea55a434b
zdm <zdm@softvisio.net>
2021-11-15 23:23:31 +0200

    fix: nvim lang

--- setup-host.sh
+++ setup-host.sh
@@ -9,52 +9,22 @@ function _setup_host() {
 
     # install common packages
     # ncurses-term required to support putty-256color term in docker
-    apt install -y bash-completion tar ca-certificates curl ncurses-term gpg
+    apt install -y bash-completion tar ca-certificates curl ncurses-term
     DEBIAN_FRONTEND=noninteractive apt install -y tzdata
 
     # load os release variables
-    . /etc/os-release
+    VERSION_CODENAME=$(source /etc/os-release && echo $VERSION_CODENAME)
 
     # install common profile
     curl -fsSLo /etc/profile.d/bash-config.sh https://raw.githubusercontent.com/softvisio/scripts/main/bashrc.sh
 
-    # install repos
-    # XXX softvisio repository
-
-    # postgres repository
-    curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /usr/share/keyrings/pgdg-archive-keyring.gpg
-    cat << EOF > /etc/apt/sources.list.d/pgdg.list
-deb [signed-by=/usr/share/keyrings/pgdg-archive-keyring.gpg] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main
+    # softvisio repository
+    cat << EOF > /etc/apt/sources.list.d/softvisio.list
+deb [trusted=yes] https://media.githubusercontent.com/media/softvisio/deb/main/repo/ stable main
 EOF
 
-    # docker repository
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    cat << EOF > /etc/apt/sources.list.d/docker.list
-deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable
-EOF
-
-    # google chrome repository
-    curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-archive-keyring.gpg
-    cat << EOF > /etc/apt/sources.list.d/google-chrome.list
-deb [signed-by=/usr/share/keyrings/google-archive-keyring.gpg] https://dl.google.com/linux/chrome/deb/ stable main
-EOF
-
-    # n
-    # XXX deb package, source /etc/profile.d/n.sh
-    export N_PREFIX=/usr/n
-    mkdir -p $N_PREFIX
-    curl -fsSL https://github.com/tj/n/archive/master.tar.gz | tar -C $N_PREFIX --strip-components=1 -xz
-    cat << EOF > /etc/profile.d/n.sh
-#!/bin/sh
-
-export N_PREFIX=/usr/n
-
-NPM_PREFIX=\$(realpath ~)/.npm/bin
-
-[[ :\$PATH: == *":\$NPM_PREFIX:"* ]] || PATH+=":\$NPM_PREFIX"
-
-[[ :\$PATH: == *":\$N_PREFIX/bin:"* ]] || PATH+=":\$N_PREFIX/bin"
-EOF
+    apt update
+    apt install -y repo-docker repo-pgsql repo-google-chrome n
 
     # upgrade installed packages to the latest versions
     apt update
