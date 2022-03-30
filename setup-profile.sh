a29f69484e0bd61873d2254a438dc27b1dbb6e7c
zdm <zdm@softvisio.net>
2022-03-30 15:13:15 +0300

    fix: update-profile command

--- setup-profile.sh
+++ setup-profile.sh
@@ -9,3 +9,5 @@ mkdir -p /etc/postgresql-common
 curl -fsSLo /etc/postgresql-common/psqlrc https://raw.githubusercontent.com/softvisio/scripts/main/profile/psqlrc
 
 . ~/.bashrc
+
+echo User bash profile updated
