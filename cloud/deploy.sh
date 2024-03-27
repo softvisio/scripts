103b128987d2f5765744e9edd47b1bb6626ffc4d
zdm <zdm@softvisio.net>
2024-03-27 16:36:16 +0200

    fix: docker swarm deplay

---
+++ cloud/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# source <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/update-dotfiles.sh)
+# /bin/bash <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/cloud/deploy.sh)
+
+set -e
+
+# docker stack deploy --with-registry-auth postgresql -c
+
+echo $1
+echo $2
