620be19a1e4ff1e914b0a38887f1c481b6f1fb89
zdm <zdm@softvisio.net>
2024-07-23 21:33:41 +0300

    fix: swarm update script

--- cloud/update.sh
+++ cloud/update.sh
@@ -2,7 +2,7 @@
 
 # source <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/cloud/update.sh)
 
-STACK_TMP_DIR=/tmp/$STACK_NAME
+DOCKER_STACK_TMP_DIR=/tmp/$DOCKER_STACK_NAME
 
 set -e
 shopt -s extglob
@@ -12,32 +12,44 @@ cd "${0%/*}"
 chmod +x **/docker-compose.yaml
 
 # create network if not exists
-[ -z $(docker network ls -q --filter name=main) ] && docker network create --driver overlay --attachable main
+# [ -z $(docker network ls -q --filter name=main) ] && docker network create --driver overlay --attachable main
 
 # prepare tmp dir
-rm -rf $STACK_TMP_DIR
-mkdir -p $STACK_TMP_DIR
+rm -rf $DOCKER_STACK_TMP_DIR
+mkdir -p $DOCKER_STACK_TMP_DIR
 
 # resolve services
 for name in !(*.disabled); do
     if [ -f "$name/docker-compose.yaml" ]; then
-        docker stack config \
+
+        cat << EOF | docker stack config \
             -c $name/docker-compose.yaml \
-            > $STACK_TMP_DIR/$name.docker-compose.yaml
+            -c - \
+            > $DOCKER_STACK_TMP_DIR/$name.docker-compose.yaml
+version: "3.9"
+
+networks:
+  network:
+  	name: $DOCKER_STACK_NETWORK_NAME
+    driver: overlay
+    attachable: true
+EOF
+
     fi
 done
 
 # remove stack
-docker stack rm --detach=false $STACK_NAME
+docker stack rm --detach=false $DOCKER_STACK_NAME
 
 # deploy stack
-ls $STACK_TMP_DIR/*.docker-compose.yaml | xargs printf -- '-c %s\n' | xargs \
+ls $DOCKER_STACK_TMP_DIR/*.docker-compose.yaml | xargs printf -- '-c %s\n' | xargs \
     docker stack deploy \
     --prune \
     --resolve-image=always \
     --detach=true \
     --with-registry-auth \
-    $STACK_NAME
+    {} \
+    $DOCKER_STACK_NAME
 
 # remove tmp dir
-rm -rf $STACK_TMP_DIR
+rm -rf $DOCKER_STACK_TMP_DIR
