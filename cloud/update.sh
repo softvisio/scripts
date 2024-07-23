e397e913ef2170dc549b5340af63475631facc8d
zdm <zdm@softvisio.net>
2024-07-23 10:10:51 +0300

    fix: swarm update script

--- cloud/update.sh
+++ cloud/update.sh
@@ -2,6 +2,8 @@
 
 # source <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/cloud/update.sh)
 
+STACK_TMP_DIR=/tmp/$STACK_NAME
+
 set -e
 shopt -s extglob
 
@@ -12,23 +14,24 @@ chmod +x **/docker-compose.yaml
 # create network if not exists
 [ -z $(docker network ls -q --filter name=main) ] && docker network create --driver overlay --attachable main
 
-# remove stack
-docker stack rm --detach=false $STACK_NAME
-
-# remove resolved compose files
-rm -rf **/docker-compose.resolved.yaml
+# prepare tmp dir
+rm -rf $STACK_TMP_DIR
+mkdir -p $STACK_TMP_DIR
 
 # resolve services
 for name in !(*.disabled); do
     if [ -f "$name/docker-compose.yaml" ]; then
         docker stack config \
             -c $name/docker-compose.yaml \
-            > $name/docker-compose.resolved.yaml
+            > $STACK_TMP_DIR/$name-docker-compose.yaml
     fi
 done
 
+# remove stack
+docker stack rm --detach=false $STACK_NAME
+
 # deploy stack
-ls **/docker-compose.resolved.yaml | xargs printf -- '-c %s\n' | xargs \
+ls $STACK_TMP_DIR/*-docker-compose.yaml | xargs printf -- '-c %s\n' | xargs \
     docker stack deploy \
     --prune \
     --resolve-image=always \
@@ -36,5 +39,5 @@ ls **/docker-compose.resolved.yaml | xargs printf -- '-c %s\n' | xargs \
     --with-registry-auth \
     $STACK_NAME
 
-# remove resolved compose files
-rm -rf **/docker-compose.resolved.yaml
+# remove tmp dir
+rm -rf $STACK_TMP_DIR
