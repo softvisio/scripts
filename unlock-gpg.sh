c39880c3ef5980c86097b3aebc99911189dd2b98
zdm <zdm@softvisio.net>
2025-06-15 05:06:52 +0300

    refactor: update ssh-crypt

--- unlock-gpg.sh
+++ unlock-gpg.sh
@@ -4,22 +4,15 @@
 
 set -e
 
-GPG_KEY_ID=$1
+ENCRYPTED_PASSPHRASE=jA0ECQMKtZzcWNcSOXf/0kUB+XWe3203WQ4Q4J2kP/FhLww7t41JF0RxcvGTfg9cD5pUCxtw8MqwDW+oh48uJ5re83WegZUOspdEIWzbaCTOKZH5DGE=
 GITHUB_USERNAME=zdm
+
+GPG_KEY_ID=$1
 
 export GPG_TTY=$(tty)
 
 # decrypt gpg passphrase
-PASSPHRASE=$(
-    cat << EOF | /usr/bin/env bash <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/ssh-crypt.sh) decrypt $GITHUB_USERNAME
------BEGIN PGP MESSAGE-----
-
-jA0ECQMKXDVY3ahL2bX/0kUBRnHUDM2IymQDwGBgGOq/Qs+1CJemX6OrcO9+2yUE
-W0Xc9tU2z9zoxekpG0lnLth4XmUf+oKjXAqVbuS917dvI9j+iRU=
-=hjeI
------END PGP MESSAGE-----
-EOF
-)
+PASSPHRASE=$(echo $ENCRYPTED_PASSPHRASE | /usr/bin/env bash <(curl -fsSL https://raw.githubusercontent.com/softvisio/scripts/main/ssh-crypt.sh) decrypt $GITHUB_USERNAME)
 
 # unlock gpg key
 echo 1 | gpg --sign \
