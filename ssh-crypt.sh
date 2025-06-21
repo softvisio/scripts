6b7f716c191b839bcb508192b0de6555e81eaa12
zdm <zdm@softvisio.net>
2025-06-21 08:18:18 +0300

    refactor: update ssh-crypt

--- ssh-crypt.sh
+++ ssh-crypt.sh
@@ -24,7 +24,7 @@ function ssh-crypt() {
         fi
 
         # create signature of github_username, that will be used as secret
-        local secret=$(ssh-keygen -Y sign -n ssh-crypt -q -f /dev/fd/4 4<<< "$public_keys" <<< "$github_username" 2> /dev/null | gpg --dearmor | basenc --base64url --wrap=0) || ""
+        local secret=$(ssh-keygen -Y sign -n ssh-crypt -q -f /dev/fd/4 4<<< "$public_keys" <<< "$github_username" 2> /dev/null | gpg --dearmor 2> /dev/null | basenc --base64url --wrap=0) || ""
 
         if [[ $secret == "" ]]; then
             echo "Private SSH key not found" >&2
