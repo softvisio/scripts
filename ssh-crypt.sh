7e4fa88eb1694451f4dd14b3982fc0ef4730969b
zdm <zdm@softvisio.net>
2025-06-24 14:37:25 +0300

    refactor: update bash scripts errors handling

--- ssh-crypt.sh
+++ ssh-crypt.sh
@@ -6,8 +6,8 @@
 
 # echo "text-to-decrypt" | /usr/bin/env bash <(curl -fsS https://raw.githubusercontent.com/softvisio/scripts/main/ssh-crypt.sh) decrypt $GITHUB_USERNAME
 
-set -Eeo pipefail
-trap 'echo -e "\n⚠  Warning: A command has failed. Line ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null || true)" >&2; return 3 2> /dev/null || exit 3' ERR
+set -Eeuo pipefail
+trap 'echo -e "⚠  Warning: A command has failed. Line ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null || true)" >&2; return 3 2> /dev/null || exit 3' ERR
 
 function ssh-crypt() {
     local operation=$1
