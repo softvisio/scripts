4fa2534c9011f6e0aad6c98fe567782bf2356141
zdm <zdm@softvisio.net>
2025-06-25 12:03:03 +0300

    style: lint

--- setup-sshd.sh
+++ setup-sshd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# NOTE https://man7.org/linux/man-pages/man5/sshd_config.5.html
+# NOTE: https://man7.org/linux/man-pages/man5/sshd_config.5.html
 
 # /usr/bin/env bash <(curl -fsS https://raw.githubusercontent.com/softvisio/scripts/main/setup-sshd.sh)
 
