bd29efb7820e5ec0cafcd6639fdaa9efbd8ce570
zdm <zdm@softvisio.net>
2025-06-25 08:15:34 +0300

    refactor: update bash scripts errors handling

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -15,14 +15,20 @@
 set -Eeuo pipefail
 trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
 
-dotfiles=$(
-    cat << JSON
+function update-dotfiles() {
+    local dotfiles=$(
+        cat << JSON
 {
     "public": "zdm/dotfiles-public",
     "private": "zdm/dotfiles-private",
     "deployment": "zdm/dotfiles-deployment"
 }
 JSON
-)
+    )
 
-source <(curl -fsS https://raw.githubusercontent.com/softvisio/scripts/main/dotfiles.sh) "$dotfiles" "$@"
+    local script
+    script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/dotfiles.sh")
+    source <(echo "$script") "$dotfiles" "$@"
+}
+
+update-dotfiles "$@"
