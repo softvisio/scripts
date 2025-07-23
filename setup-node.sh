3a4c70dec63ad6b517a72898475c71fcfd27e542
zdm <zdm@softvisio.net>
2025-07-23 11:34:53 +0300

    fix: update setup-node

--- setup-node.sh
+++ setup-node.sh
@@ -6,14 +6,6 @@
 set -Eeuo pipefail
 trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
 
-# if [[ ! -x "$(command -v node)" && -x "$(command -v n)" ]]; then
-#     n lts
-# fi
-
-# make global modules loadable
-# rm -rf ~/.node_modules
-# ln -s ~/.npm/lib/node_modules ~/.node_modules
-
 # update node_modules
 # npm install --global npm
 # npm update --global --force --unsafe
