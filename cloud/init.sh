c72aba13c27833e39b0a9dac281ace82362fbd4f
zdm <zdm@softvisio.net>
2026-04-30 14:09:10 +0300

    feat: add cloud init.sh

--- cloud/init.sh
+++ cloud/init.sh
@@ -3,6 +3,26 @@
 set -Eeuo pipefail
 trap 'echo "⚠  Error ($0:$LINENO, exit code: $?): $BASH_COMMAND" >&2' ERR
 
+function _setup_instance() {
+    local script
+
+    # setup host
+    script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/setup-host.sh")
+    bash <(echo "$script")
+
+    # setup timesync
+    script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/setup-timesync.sh")
+    bash <(echo "$script")
+
+    # install ssh public key
+    script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/install-ssh-public-key.sh")
+    bash <(echo "$script")
+
+    # setup sshd
+    script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/setup-sshd.sh")
+    bash <(echo "$script")
+}
+
 function _setup_hostname() {
     local name
     name=$(curl -fsS -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/name")
@@ -24,21 +44,8 @@ function _init_docker() {
     fi
 }
 
-# setup host
-script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/setup-host.sh")
-bash <(echo "$script")
-
-# setup timesync
-script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/setup-timesync.sh")
-bash <(echo "$script")
-
-# install ssh public key
-script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/install-ssh-public-key.sh")
-bash <(echo "$script")
-
-# setup sshd
-script=$(curl -fsS "https://raw.githubusercontent.com/softvisio/scripts/main/setup-sshd.sh")
-bash <(echo "$script")
+# setup instance
+_setup_instance
 
 # setup hostname
 _setup_hostname
