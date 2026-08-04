65d365b6a973c378c9fd3b6e9af6ddf14c7f952a
zdm <zdm@softvisio.net>
2026-08-04 16:00:07 +0300

    chore: rename zdm/toolset to ztools

--- setup-node.sh
+++ setup-node.sh
@@ -24,7 +24,7 @@ pushd /var/local/corejslib/core
 npm link --dangerously-allow-all-scripts --force
 popd
 
-pushd /var/local/zdm/toolset
+pushd /var/local/zdm/ztools
 npm link --dangerously-allow-all-scripts --force
 popd
 
